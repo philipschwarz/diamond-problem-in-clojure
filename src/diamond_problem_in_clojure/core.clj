f72b3768e1afa205eaf48c34ffcce99a74d94a00
Philip Schwarz <philip.johann.schwarz@gmail.com>
2014-12-05 21:45:07 +0000

    final tidy-up (for now?)

--- src/diamond_problem_in_clojure/core.clj
+++ src/diamond_problem_in_clojure/core.clj
@@ -6,6 +6,8 @@
 
 (defn dashes [n] (repeat n \-))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn drop-first-column [sequence-of-sequences-of-chars]
   (map rest sequence-of-sequences-of-chars))
 
@@ -16,13 +18,25 @@
   (map reverse sequence-of-sequences-of-chars))
 
 (defn core-letter-pattern-for [letter]
-  (concat (dashes (dec (position-of letter))) (list letter)))
+  (concat
+    (dashes (dec (position-of letter)))
+    (list letter)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn core-right-quadrant-pattern-for [letter]
   (map core-letter-pattern-for (take (position-of letter) alphabet)))
 
 (defn trailing-dashes-appender-for [max-dash-count]
   (fn [index line] (concat line (dashes (dec (- max-dash-count index))))))
+
+(defn flip-bottom-up [top-half-of-diamond]
+  (reverse top-half-of-diamond))
+
+(defn join-together-side-by-side [top-left-quadrant top-right-quadrant]
+  (map concat top-left-quadrant top-right-quadrant))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn create-top-right-quadrant-for [letter]
   (let [number-of-letters (position-of letter)]
@@ -33,33 +47,28 @@
        drop-first-column
        reverse-every-row))
 
-(defn join-together-side-by-side [top-left-quadrant top-right-quadrant]
-  (map concat top-left-quadrant top-right-quadrant))
-
 (defn create-top-half-of-diamond-with [top-left-quadrant top-right-quadrant]
   (join-together-side-by-side top-left-quadrant top-right-quadrant))
-
-(defn flip-bottom-up [top-half-of-diamond]
-  (reverse top-half-of-diamond))
-
-(defn put-on-top-one-another [top-half-of-diamond bottom-half-of-diamond]
-  (concat top-half-of-diamond bottom-half-of-diamond))
 
 (defn create-bottom-half-of-diamond-from [top-half-of-diamond]
   (drop-first-row (flip-bottom-up top-half-of-diamond)))
 
-(defn stringify [char-sequences]
+(defn put-one-on-top-of-the-other [top-half-of-diamond bottom-half-of-diamond]
+  (concat top-half-of-diamond bottom-half-of-diamond))
+
+(defn strings-for [char-sequences]
   (map #(apply str %) char-sequences))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn print-diamond [letter]
   (let [top-right-quadrant (create-top-right-quadrant-for letter)
         top-left-quadrant (create-top-left-quadrant-from top-right-quadrant)
         top-half-of-diamond (create-top-half-of-diamond-with top-left-quadrant top-right-quadrant)
         bottom-half-of-diamond (create-bottom-half-of-diamond-from top-half-of-diamond)
-        diamond (put-on-top-one-another top-half-of-diamond bottom-half-of-diamond)]
-    (doseq [line (stringify diamond)]
+        diamond (put-one-on-top-of-the-other top-half-of-diamond bottom-half-of-diamond)]
+    (doseq [line (strings-for diamond)]
       (println line))))
 
 (defn -main []
   (print-diamond \E))
-
